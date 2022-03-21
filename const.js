1334e19075a9433dd12c5d5d72ff56f94d10c4f5
Nikola Cukic <nikolacukic@gmail.com>
2022-03-21 10:43:02 +0100

    Fix id field naming

--- const.js
+++ const.js
@@ -1,7 +1,7 @@
 DB_SQL_PATH = 'my-db.sql';
 DB_PATH = 'my.db';
 
-GET_ALL_USERS_QUERY = 'SELECT id, username FROM user';
+GET_ALL_USERS_QUERY = 'SELECT _id, username FROM user';
 INSERT_USER_QUERY = 'INSERT INTO user (username) VALUES (?)';
 
 module.exports = {
